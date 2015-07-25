46f02d9a244ea48a7e092aa3b5913250c20ae097
klimekk <kazimirka@shaw.ca>
2015-07-25 14:37:56 -0600

    results of Programming work for Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,42 @@
 ## functions do
 
 ## Write a short comment describing this function
+## function to built list overhead with matrix and all functionality
+## get allow you to get matrix - result of setmatrix operation
+## set allow you to set matrix 
+## m is a value of inverse matrix stored
+## getmatrix allow you to get matrix - result of setmatrix operation
+## setmatrix allow you to operate on matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m<-NULL
+      set<-function(y){
+            x<<-y
+            m<<-NULL
+      }
+      get<-function() x
+      setmatrix<-function(solve) m<<- solve
+      getmatrix<-function() m
+      list(set=set, get=get,
+           setmatrix=setmatrix,
+           getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
+## function to manipulate components of makeCacheMatrix
+## if value is cashed - return the result of getmatrix, otherwise - perform solve and save to makeCacheMatrix for future reusage
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x=matrix(), ...) {
+      ## Return a matrix that is the inverse of 'x'
+      m<-x$getmatrix()
+      if(!is.null(m)){
+            message("getting cached data")
+            return(m)
+      }
+      matrix<-x$get()
+      m<-solve(matrix, ...)
+      x$setmatrix(m)
+      m
 }
